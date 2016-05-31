d827ab6641411887bb88d0bf6768812410305f40
Mac <macchiang@gmail.com>
2016-05-31 15:42:29 +0800

    Update crawl.py
    make it more easy

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+// craw ptt post
 import requests
 from bs4 import BeautifulSoup
 req=requests.get("https://www.ptt.cc/bbs/HatePolitics/index.html")
@@ -5,6 +6,5 @@ html=req.text.encode('utf-8')
 soup=BeautifulSoup(html,"html.parser")
 for rents in soup.select('.r-ent'):
     print rents.select('.title')[0].text.encode('utf-8')
-    for metas in rents.select('.meta'):
-        print rents.select('.date')[0].text.encode('utf-8')
-        print rents.select('.author')[0].text.encode('utf-8')
+    print rents.select('.date')[0].text.encode('utf-8')
+    print rents.select('.author')[0].text.encode('utf-8')
