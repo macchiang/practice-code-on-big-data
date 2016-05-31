34c4a41be0d028f908004e4d9e02e1b4813d13ca
Mac <macchiang@gmail.com>
2016-05-31 17:13:34 +0800

    Add hyperlink parser

--- apple/apple/spiders/crawler.py
+++ apple/apple/spiders/crawler.py
@@ -5,6 +5,8 @@ class AppleCrawler(scrapy.Spider):
 	name='apple'
 	start_urls=['http://www.appledaily.com.tw/realtimenews/section/new/']
 	def parse(self,response):
+		domain_name="http://www.appledaily.com.tw"
 		res=BeautifulSoup(response.body)
 		for news in res.select('.rtddt'):
 			print news.select('h1')[0].text
+			print news.select('a')[0]['href']
